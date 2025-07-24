5eccc5fe8a0d3b562dc6c3156b3bdc5ac0b4f880
AlanRoy25 <alanroythomas4@gmail.com>
2025-07-24 17:17:40 +0530

    module cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,13 @@ import { Route, Switch } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-// Only one route: homepage
+// Page imports
 import HomePage from "@/pages/Home";
+import ContactPage from "@/components/Contact";
+import AboutPage from "@/components/About";
+import ServicesPage from "@/components/Services";
+import WorksPage from "@/components/Works";
+import WhyChooseUsPage from "@/components/Whychooseus";
 
 function App() {
   return (
@@ -11,8 +16,11 @@ function App() {
       <Toaster />
       <Switch>
         <Route path="/" component={HomePage} />
-        {/* Optional: 404 Route */}
-        <Route>Page not found</Route>
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/services" component={ServicesPage} />
+        <Route path="/works" component={WorksPage} />
+        <Route path="/whychooseus" component={WhyChooseUsPage} />
       </Switch>
     </TooltipProvider>
   );
